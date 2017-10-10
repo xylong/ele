6b63c98241dbba80898607e7f51e70dc8b51af28
xylong <416319808@qq.com>
2017-10-10 14:38:14 +0800

    stylus实现像素border

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,54 @@
 <template>
     <div>
         <v-header></v-header>
-        <div class="tab">
-            <div class="tab-item"><router-link to="/goods">商品</router-link></div>
-            <div class="tab-item"><router-link to="/ratings">评论</router-link></div>
-            <div class="tab-item"><router-link to="/sellers">商家</router-link></div>
+        <div class="tab border-1px">
+            <div class="tab-item" v-for="v in menu"><router-link :to="v.url">{{v.name}}</router-link></div>
         </div>
-        <div class="content">
-            <router-view></router-view>
-        </div>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
 import header from '@/components/header/header.vue'
 export default {
-  name: 'app',
-  components: {
-      'v-header': header
-  }
+    name: 'app',
+    data() {
+        return {
+            menu: [{
+                url: '/goods',
+                name: '商品'
+            }, {
+                url: '/ratings',
+                name: '评论'
+            }, {
+                url: '/sellers',
+                name: '商家'
+            }]
+        }
+    },
+    components: {
+        'v-header': header
+    }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import './common/stylus/mixin.styl'
+
 .tab
     display: flex
     width: 100%
     height: 40px
     line-height: 40px
+    // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+    border-1px(rgba(1, 17, 27, 0.1))
     .tab-item
         flex: 1
         text-align: center
+        & > a
+            display: block
+            font-size: 14px
+            color: rgb(77, 85, 93)
+            &.active
+                color: rgb(240, 20, 20)
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import goods from '@/components/goods/goods'
 import ratings from '@/components/ratings/ratings'
 import sellers from '@/components/sellers/sellers'
 
+import 'common/stylus/index.styl'
+
 Vue.use(Router)
 
 export default new Router({
