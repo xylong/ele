5643daf4db9f94648bb587342a0a31e0f483c9f8
xylong <416319808@qq.com>
2017-10-11 16:40:50 +0800

    星星样式调整

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -6,9 +6,9 @@
 
 <script>
 const LEN = 5
-const ON = 'ON'
-const HALF = 'HALF'
-const OFF = 'OFF'
+const ON = 'on'
+const HALF = 'half'
+const OFF = 'off'
 
 export default {
 	name: 'star',
@@ -56,39 +56,43 @@ export default {
 	.star-item
 		display: inline-block
 		background-repeat: no-repeat
-	&:last-child
-		margin-right: 0
 	&.star-48
 		.star-item
 			width: 20px
 			height: 20px
 			margin-right: 22px
 			background-size: 20px 20px
-			&:on
+			&:last-child
+				margin-right: 0
+			&.on
 				bg-image('star48_on')
 			&.half
 				bg-image('star48_half')
 			&.off
 				bg-image('star48_off')
-	&star-36
+	&.star-36
 		.star-item
 			width: 15px
 			height: 15px
 			margin-right: 16px
 			background-size: 15px 15px
-			&:on
+			&:last-child
+				margin-right: 0
+			&.on
 				bg-image('star36_on')
 			&.half
 				bg-image('star36_half')
 			&.off
 				bg-image('star36_off')
-	&star-24
+	&.star-24
 		.star-item
 			width: 10px
 			height: 10px
 			margin-right: 3px
 			background-size: 10px 10px
-			&:on
+			&:last-child
+				margin-right: 0
+			&.on
 				bg-image('star24_on')
 			&.half
 				bg-image('star24_half')
