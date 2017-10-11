7cb9c26fd910590db79ce657d6b34004b1b5a2b5
xylong <416319808@qq.com>
2017-10-11 16:59:31 +0800

    横线

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -37,7 +37,14 @@
         	<div class="detail-wrapper clearfix">
         		<div class="detail-main">
 					<h1 class="name" v-text="seller.name"></h1>
-					<star :size="48" :score="seller.score"></star>
+					<div class="star-wrapper">
+ 						<star :size="48" :score="seller.score"></star>
+					</div>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">优惠信息</div>
+						<div class="line"></div>
+					</div>
         		</div>
         	</div>
         	<div class="detail-close">
@@ -211,6 +218,23 @@ export default {
 					text-align: center
 					font-size: 16px
 					font-weight: 700
+				.star-wrapper
+					margin-top: 18px
+					padding: 2px 0
+					text-align: center
+				.title
+					display: flex
+					width: 80%
+					margin: 28px auto 24px auto
+					.line
+						flex: 1
+						position: relative
+						top: -6px
+						border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+					.text
+						padding: 0 12px
+						font-weight: 700
+						font-size: 14px
 		.detail-close
 			position: relative
 			width: 32px
