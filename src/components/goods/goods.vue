08000220951ec3dc0047afa0d9f69f8f35a661c0
xylong <416319808@qq.com>
2017-10-20 16:45:06 +0800

    商品组件右侧布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -9,7 +9,32 @@
 				</li>
 			</ul>
 		</div>
-		<div class="foods-wrapper"></div>
+		<div class="foods-wrapper">
+			<ul>
+				<li v-for="item in goods" class="food-list">
+					<h1 class="title" v-text="item.name"></h1>
+					<ul>
+						<li v-for="food in item.foods" class="foot-item">
+							<div class="icon">
+								<img :src="food.icon">
+							</div>
+							<div class="content">
+								<h2 class="name" v-text="food.name"></h2>
+								<p class="desc" v-text="food.description"></p>
+								<div class="extra">
+									<span>月售{{food.sellCount}}</span>
+									<span>好评率{{food.rating}}%</span>
+								</div>
+								<div class="price">
+									<span>￥{{food.price}}</span>
+									<span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
