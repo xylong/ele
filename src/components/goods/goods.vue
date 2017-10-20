56a55d53f0544948ef6cfa98982d1f691b1d4a60
xylong <416319808@qq.com>
2017-10-20 16:19:48 +0800

    商品左侧菜单布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,14 @@
 <template lang="html">
 	<div class="goods">
-		<div class="menu-wrapper"></div>
+		<div class="menu-wrapper">
+			<ul>
+				<li v-for="item in goods" class="menu-item">
+					<span class="text border-1px">
+						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+					</span>
+				</li>
+			</ul>
+		</div>
 		<div class="foods-wrapper"></div>
 	</div>
 </template>
@@ -17,7 +25,8 @@ export default {
 	},
 	data() {
 		return {
-			goods: {}
+			goods: {},
+			classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee']
 		}
 	},
 	mounted() {
@@ -33,6 +42,8 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixin'
+
 .goods
 	display flex
 	position absolute
@@ -44,6 +55,36 @@ export default {
 		flex 0 0 80px
 		width 80px
 		background #f3f5f7
+		.menu-item
+			display table
+			height 54px
+			width 56px
+			padding 0 12px
+			line-height 14px
+			.icon
+				display inline-block
+				vertical-align top
+				width 12px
+				height 12px
+				margin-right 2px
+				background-size 12px 12px
+				background-repeat no-repeat
+				&.decrease
+					bg-image('decrease_3')
+				&.discount
+					bg-image('discount_3')
+				&.guarantee
+					bg-image('guarantee_3')
+				&.invoice
+					bg-image('invoice_3')
+				&.special
+					bg-image('special_3')
+			.text
+				display table-cell
+				width 56px
+				vertical-align middle
+				font-size 12px
+				border-1px(rgba(7, 17, 27, 0.1))
 	.foods-wrapper
 		flex 1
 </style>
