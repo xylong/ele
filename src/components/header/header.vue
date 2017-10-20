87ede7fe44643be012321e579551319830645203
xylong <416319808@qq.com>
2017-10-20 14:13:17 +0800

    弹出层优惠信息样式 导入优惠信息图片

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -45,6 +45,20 @@
 						<div class="text">优惠信息</div>
 						<div class="line"></div>
 					</div>
+					<ul v-if="seller.supports" class="supports">
+						<li class="support-item" v-for="(item, index) in seller.supports">
+							<span class="icon" :class="classMap[seller.supports[index].type]"></span>
+							<span class="text" v-text="seller.supports[index].description"></span>
+						</li>
+					</ul>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">商家公告</div>
+						<div class="line"></div>
+					</div>
+					<div class="bulletin">
+						<p class="content" v-text="seller.bulletin"></p>
+					</div>
         		</div>
         	</div>
         	<div class="detail-close">
@@ -235,6 +249,36 @@ export default {
 						padding: 0 12px
 						font-weight: 700
 						font-size: 14px
+				.supports
+					width: 80%
+					margin: 0 auto
+					.support-item
+						padding: 0 12px
+						margin-bottom: 12px
+						font-size: 0
+						&:last-child
+							margin-bottom: 0
+						.icon
+							display: inline-block
+							width: 16px
+							height: 16px
+							vertical-align: top
+							margin-right: 6px
+							background-size: 16px
+							background-repeat: no-repeat
+							&.decrease
+								bg-image('decrease_2')
+							&.discount
+								bg-image('discount_2')
+							&.guarantee
+								bg-image('guarantee_2')
+							&.invoice
+								bg-image('invoice_2')
+							&.special
+								bg-image('special_2')
+						.text
+							line-height: 16px
+							font-size: 12px
 		.detail-close
 			position: relative
 			width: 32px
