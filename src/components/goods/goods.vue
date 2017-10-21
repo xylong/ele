c066cb2d063d9ebf3c9c3799f0103bbe11ca00ee
xylong <41631808@qq.com>
2017-10-21 14:08:00 +0800

    右侧食品列表样式

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -14,20 +14,20 @@
 				<li v-for="item in goods" class="food-list">
 					<h1 class="title" v-text="item.name"></h1>
 					<ul>
-						<li v-for="food in item.foods" class="foot-item">
+						<li v-for="food in item.foods" class="food-item border-1px">
 							<div class="icon">
-								<img :src="food.icon">
+								<img :src="food.icon" width="57" height="57">
 							</div>
 							<div class="content">
 								<h2 class="name" v-text="food.name"></h2>
 								<p class="desc" v-text="food.description"></p>
 								<div class="extra">
-									<span>月售{{food.sellCount}}</span>
+									<span class="count">月售{{food.sellCount}}</span>
 									<span>好评率{{food.rating}}%</span>
 								</div>
 								<div class="price">
-									<span>￥{{food.price}}</span>
-									<span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+									<span class="now">￥{{food.price}}</span>
+									<span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
 								</div>
 							</div>
 						</li>
@@ -112,4 +112,51 @@ export default {
 				border-1px(rgba(7, 17, 27, 0.1))
 	.foods-wrapper
 		flex 1
+		.title
+			padding-left 14px
+			line-height 26px
+			height 26px
+			border-left 2px solid #d9dde1
+			font-size 12px
+			color rgb(147, 153, 159)
+			background #f3f5f7
+		.food-item
+			display flex
+			margin 18px
+			padding-bottom 18px
+			border-1px(rgba(7, 17, 27, 0.1))
+			&:last-child
+				border-none
+				margin-bottom 0
+			.icon
+				flex 0 0 57px
+				margin-right 10px
+			.content
+				flex 1
+				.name
+					margin 2px 0 18px 0
+					height 14px
+					line-height 14px
+					font-size 14px
+					color rgb(7, 17, 27)
+				.desc, .extra
+					line-height 10px
+					font-size 10px
+					color rgb(147, 153, 159)
+				.desc
+					margin-bottom 8px
+				.extra
+					&.count
+						margin-right 12px
+				.price
+					font-weight 700
+					line-height 24px
+					.now
+						margin-right 18px
+						font-size 14px
+						color rgb(240 20 20 )
+					.old
+						text-decoration line-through
+						font-size 10px
+						color rgb(147, 153, 159)
 </style>
